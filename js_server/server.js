5b0e41f005fa9883310fb37ba0356fdfdaa1cfe6
dor219 <dor219@lehigh.edu>
2023-09-30 11:20:22 -0400

    formatting for readability

--- js_server/server.js
+++ js_server/server.js
@@ -4,7 +4,8 @@ const pg = require('pg');
 const app = express();
 const port = 3000;
 
-// removed url to push to git, because I don't know how to push passwords safely in NodeJS yet
+// removed url to push to git, because I don't know how to
+// properly separate passwords from source code safely in NodeJS yet
 const conString = "MY_ELEPHANTSQL_URL";
 const client = new pg.Client(conString);
 
@@ -18,19 +19,18 @@ app.listen(port, () => {
 
 // listen for incoming POST requests from Board
 app.post('/data', (req, res) => {
-    let today = new Date();
-    let paddedMonth   = (today.getMonth()+1).toLocaleString(undefined, {minimumIntegerDigits: 2});
-    let paddedDay     = today.getDate().toLocaleString(undefined, {minimumIntegerDigits: 2});
-    let paddedHours   = today.getHours().toLocaleString(undefined, {minimumIntegerDigits: 2});
-    let paddedMinutes = today.getMinutes().toLocaleString(undefined, {minimumIntegerDigits: 2});
-    let paddedSeconds = today.getSeconds().toLocaleString(undefined, {minimumIntegerDigits: 2});
-    let date = today.getFullYear() + "-" + paddedMonth + "-" + paddedDay;
-    let time = paddedHours + ":" + paddedMinutes + ":" + paddedSeconds;
-
+    
+    // parses values from incoming data from Board, 
+    // and current timestamp to send to the database
+    dateTime = getDateTime();
     const data = req.body;
 
-
-    let my_query = `INSERT INTO temprh (date, time, temp, rh) VALUES ('${date}', '${time}', '${data.temp}', '${data.rh}')`;
+    // builds SQL query
+    let my_query = "INSERT INTO temprh (date, time, temp, rh) VALUES ("
+                    + `'${dateTime.date}',`
+                    + `'${dateTime.time}',`
+                    + `'${data.temp}',`
+                    + `'${data.rh}')`;
     console.log("Attempting to run " + my_query);
     
     client.connect(function(err) {
@@ -48,3 +48,19 @@ app.post('/data', (req, res) => {
     
     res.status(200).send("Done");
   });
+
+
+// returns an Object containing 'date' and 'time'
+// of the form {date:"YYYY-MM-DD", time:"hh:mm:ss"}
+function getDateTime() {
+  let today = new Date();
+  let paddedMonth   = (today.getMonth()+1).toLocaleString(undefined, {minimumIntegerDigits: 2});
+  let paddedDay     = today.getDate().toLocaleString(undefined, {minimumIntegerDigits: 2});
+  let paddedHours   = today.getHours().toLocaleString(undefined, {minimumIntegerDigits: 2});
+  let paddedMinutes = today.getMinutes().toLocaleString(undefined, {minimumIntegerDigits: 2});
+  let paddedSeconds = today.getSeconds().toLocaleString(undefined, {minimumIntegerDigits: 2});
+  let date = today.getFullYear() + "-" + paddedMonth + "-" + paddedDay;
+  let time = paddedHours + ":" + paddedMinutes + ":" + paddedSeconds;
+  
+  return {date:date, time:time};
+}
