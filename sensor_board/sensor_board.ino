110df77715d2ea9068bd50c33de3fc14b4d92b6a
dor219 <dor219@lehigh.edu>
2023-10-08 09:18:43 -0400

    refactored for readability / quick switch between local and remote hosts for testing deployment

--- sensor_board/sensor_board.ino
+++ sensor_board/sensor_board.ino
@@ -4,35 +4,32 @@
 // seriously enough to start spending money on hardware.
 
 #include <AHT20.h>
-#include <Wire.h>
 #include "WiFiEsp.h"
 #include "SoftwareSerial.h"
-
 #include "WiFiCredentials.h"
-char ssid[] = SSID;              // imported from WiFiCredentials.h
-char pass[] = PASSWORD;          // imported from WiFiCredentials.h
-int status = WL_IDLE_STATUS;     // the Wifi radio's status
+
 SoftwareSerial Serial1(6, 7);    // RX, TX
 
 WiFiEspClient client;
 AHT20 aht20;
 
-#define HOST "192.168.0.39"
-int port = 3000;
+// a variable so I can quickly swap between "pushing to local/remote backend" modes
+// for testing why one works and the other doesn't
+bool local = true;
 
+// set HOST based on value of bool descripter "local"
+// hostnames imported from WiFICredentials.h for security
+#define HOST local ? LOCAL_HOST : REMOTE_HOST
+int port = 3000;
 
 void setup()
 {
     pinMode(LED_BUILTIN, OUTPUT);
-    Wire.begin();
 
     // if unable to read the sensor, blink furiously
     if (aht20.begin() == false) {
-        byte hilow = 0;
         for(;;) {
-            hilow = hilow == 0 ? 1 : 0; 
-            digitalWrite(LED_BUILTIN, hilow);
-            delay(100);
+            blink(LED_BUILTIN, 2);
         }
     }
 
@@ -41,77 +38,78 @@ void setup()
     WiFi.init(&Serial1);
 
     // attempt to connect to WiFi network
+    int status = WL_IDLE_STATUS;     // default Wifi radio's status
     while (status != WL_CONNECTED) {
-        status = WiFi.begin(ssid, pass);
+        // SSID and PASSWORD imported from WiFiCredentials.h
+        status = WiFi.begin(SSID, PASSWORD);
     }
   
     // blink to show it's live
-    byte hilow = 0;
-    for(int i = 0; i < 10; i++) {
-        hilow = hilow == 0 ? 1 : 0; 
-        digitalWrite(LED_BUILTIN, hilow);
-        delay(100);
-    }
+    blink(LED_BUILTIN, 5);
 }
-
 
 void loop()
 {
     // try to connect to the host, and blink to show if it can't
     if (!client.connect(HOST, port)) {
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(100);
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(200);
+        blink(LED_BUILTIN, 3);
         return;
     }
 
-    while (client.connected()) {
-
-        // intialize real data values to send, save as string
-        // since AVR controllers don't support sprintf for floats
-        float temp = 32.0 + (1.8 * aht20.getTemperature());
-        float rh   = aht20.getHumidity();
-        char str_temp[8];
-        char str_rh[5];
-        dtostrf(temp, 4, 2, str_temp);
-        dtostrf(rh, 4, 2, str_rh);
-
-        // The HTTP command sent by curl to my ExpressJS app on Vercel
-        // I want to emulate this Request to send it from this ESP client:
-
-        // > POST /data HTTP/2
-        // > Host: <MY_APP_HOSTNAME>
-        // > user-agent: curl/7.81.0
-        // > accept: */*
-        // > content-type: application/json
-        // > content-length: 25
-
-        // But for now, this request hits my ExpressJS server when locally hosted
-        client.println("\r\nPOST /data HTTP/1.1");
-        char host[];
-        sprintf(host, "Host: %s:%d", HOST, port);
-        client.println(host);
-        client.println("User-Agent: Doug's TempRH Board/1.0");
-        client.println("Accept: */*");
-        client.println("Content-Type: application/json");
-
-        // format data as JSON to send
-        char data[36];
-        sprintf(data, "{\"temp\":\"%s\",\"rh\":\"%s\"}", str_temp, str_rh);
-
-        // compute length of the data JSON string
-        int content_len = 0;
-        while(data[content_len])
-            content_len++;
-
-        // format content-length header
-        char content_len_str[23];
-        sprintf(content_len_str, "Content-Length: %d\r\n\r\n", content_len + 2);
-
-        client.println(content_len_str);
-        client.println(data);
-
+    while(client.connected()) {
+        sendPOST();
+        blink(LED_BUILTIN, 2);
         delay(5000);
     }
+
+    client.stop();
+}
+
+void blink(byte pin, int times) {
+    byte hilow = 0;
+    for(int i = 0; i < 2 * times; i++) {
+        hilow = hilow == 0 ? 1 : 0; 
+        digitalWrite(pin, hilow);
+        delay(100);
+    }
+}
+
+
+// send the request over the client
+void sendPOST() {
+
+    // intialize real data values to send, save as string
+    // since AVR controllers don't support sprintf for floats
+    // float temp = 32.0 + (1.8 * aht20.getTemperature());
+    // float rh   = aht20.getHumidity();
+    // char str_temp[8];
+    // char str_rh[5];
+    // dtostrf(temp, 4, 2, str_temp);
+    // dtostrf(rh, 4, 2, str_rh);
+    char str_temp[5] = "00.0";
+    char str_rh[5] = "00.0";     
+
+    // format data as JSON to send
+    char data[36];
+    sprintf(data, "{\"temp\":\"%s\",\"rh\":\"%s\"}\r\n", str_temp, str_rh);
+
+    // This is the HTTP request taken from `curl` output when posting via linux cli
+    // and successfully hits the database every time
+    // > POST /data HTTP/2
+    // > Host: <MY_APP_HOSTNAME>
+    // > user-agent: curl/7.81.0
+    // > accept: */*
+    // > content-type: application/json
+    // > content-length: 25
+
+    client.println("\r\nPOST /data HTTP/1.1");
+    client.print("Host: ");
+    client.println(HOST);
+    client.println("User-Agent: Doug's TempRH Board/1.0");
+    client.println("Accept: */*");
+    client.println("Connection: close");
+    client.println("Content-Type: application/json");
+    client.println("Content-Length: 29");
+    client.println();
+    client.println(data);
 }
