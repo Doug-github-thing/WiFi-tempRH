010f1135e48799b5126151a0ab6dd8614dc082e8
dor219 <dor219@lehigh.edu>
2023-11-26 15:43:54 -0500

    added chart to display hour data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
-import snowflake from './snowflaketime.svg';
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import API from './api/API.js';
+import snowflake from './snowflaketime.svg'; // Snowflake icon
+import API from './api/API.js'; // Backend request routes
+import { LineChart, Line, ResponsiveContainer, XAxis, YAxis, Legend } from 'recharts'; // Historical data visualization
 
 function App() {
 
@@ -15,25 +16,56 @@ function App() {
     API.getHour().then(json => setData(json));
   }, []);
 
+  // Returns an array of the form [lower, upper] (ie [32, 80] for graph range)
+  // for the specified key, either "temp" or "rh"
+  function getYAxisRange (key) {
+    if (!data)
+      return [0,0];
+
+    let dataMap;
+    // Isolates temp data
+    if (key === "temp")
+      dataMap = data.map(dataPoint => Number(dataPoint.temp));
+    else if (key === "rh")
+      dataMap = data.map(dataPoint => Number(dataPoint.rh));
+
+    // Looks for the lowest and highest values in the data map
+    let min = Infinity, max = -Infinity;
+    let value = 0;
+    for (value of dataMap) {
+      if (value<min) min = Number(value);
+      if (max<value) max = Number(value);
+    }
+    
+    // Round the boundaries and return
+    return [Math.floor(min) - 3, Math.ceil(max) + 3];
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={snowflake} className="App-logo" alt="SMOW" />
         
         {/* If no current data, display "Loading". If there is current data, display it! */}
-        {!current ? 'Loading...' :
+        {!current ? "Loading..." :
           <div className="dataWrapper">
             <p>Temp: {current.temp}&deg;F</p>
             <p>RH: {current.rh}%</p>
           </div>
         }
 
-        {!data ? 'Loading...' :
-          <div className="dataWrapper">
-            <p>Timestamps: {data.map(dataPoint => dataPoint.timestamp)}</p>
-            <p>Temps: {data.map(dataPoint => dataPoint.temp)}</p>
-            <p>Humidities: {data.map(dataPoint => dataPoint.rh)}</p>
-          </div>
+        {!data ? "Loading" :
+          <>
+            <div style={{color: "#BADBED"}}>Past hour data:</div>
+            <LineChart width={800} height={400} data={data}>
+              <Line yAxisId="left" dataKey="temp" stroke="#61dafb" dot={false} />
+              <YAxis yAxisId="left" stroke="#61dafb" domain={getYAxisRange("temp")} /> 
+              <Line yAxisId="right" stroke="#BADBED" type="monotone" dataKey="rh" dot={false} />
+              <YAxis yAxisId="right" stroke="#BADBED" domain={getYAxisRange("rh")} orientation={"right"} /> 
+              <XAxis dataKey="timestamp" stroke="#BADBED" />
+              <Legend />
+            </LineChart>
+          </>
         }
 
       </header>
