bb58a1541c45b409c44c8db8f6b1fa06348eb281
dor219 <dor219@lehigh.edu>
2023-12-14 22:44:52 -0500

    added list view button functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ function App() {
 
   const [current, setCurrent] = useState(null); // tracks most recent data
   const [data, setData] = useState(null);       // tracks all data for selected sensor in specified range
+  const [inListView, setInListView] = useState(false); // tracks if graph is in list view
 
 
   // Reset displayed data on screen on load
@@ -47,6 +48,22 @@ function App() {
   }
 
 
+  const toggleListView = () => {
+    inListView ? setInListView(false) : setInListView(true);
+  }
+
+  // Converts a unix timestamp to a human readable M/D/Y HH:MM
+  const convertUnixTimestampFull = (unix_timestamp) => {
+    const date = new Date(unix_timestamp * 1000);
+    const day = date.getDay();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    const hours = date.getHours();
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    return `${month}/${day}/${year} ${hours}:${minutes}`;
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -61,28 +78,48 @@ function App() {
 
         <img src={snowflake} className="App-logo" alt="SMOW" />
 
-        {!data ? "" :
+        {!data ? "" : // If there's data, render it!
           <>
-            <div style={{color: "#BADBED"}}>Past 24 hours:</div>
-            <ResponsiveContainer width="100%" aspect={2}>
-              <LineChart data={data}
-                margin={{ top: 10, right: 10, left: 10, bottom: 50 }}>      
-                <Line yAxisId="left" dataKey="temp" stroke="#61dafb" dot={false} />
-                <YAxis yAxisId="left" stroke="#61dafb" domain={getYAxisRange("temp")} /> 
-                <Line yAxisId="right" stroke="#BADBED" type="monotone" dataKey="rh" dot={false} />
-                <YAxis yAxisId="right" stroke="#BADBED" domain={getYAxisRange("rh")} orientation={"right"} /> 
-                <XAxis dataKey="timestamp" stroke="#BADBED" angle={-30} textAnchor="end" />
-                <Tooltip content={<CustomTooltip />} cursor={{ fill: "transparent" }} />
-                <Legend layout="horizontal" verticalAlign="top" align="center"  />
-              </LineChart>
-            </ResponsiveContainer>
+            <div>
+              <div style={{float:"left"}}>Past 24 hours:      </div>
+              <button onClick={toggleListView}>{inListView ? "Show Graph" : "Show List"}</button>
+            </div>
+
+            {!inListView ?
+              // If we're not in list view
+              <ResponsiveContainer className={"GraphView"} width="100%" aspect={ 2 }>
+                <LineChart data={ data }
+                  margin={{ top: 10, right: 10, left: 10, bottom: 50 }}>      
+                  <Line yAxisId="left" dataKey="temp" stroke="#61dafb" dot={ false } />
+                  <YAxis yAxisId="left" stroke="#61dafb" domain={ getYAxisRange("temp") } /> 
+                  <Line yAxisId="right" stroke="#BADBED" type="monotone" dataKey="rh" dot={ false } />
+                  <YAxis yAxisId="right" stroke="#BADBED" domain={ getYAxisRange("rh") } orientation={ "right" } /> 
+                  <XAxis dataKey="timestamp" stroke="#BADBED" angle={ -30 } textAnchor="end" />
+                  <Tooltip content={ <CustomTooltip /> } cursor={{ fill: "transparent" }} />
+                  <Legend layout="horizontal" verticalAlign="top" align="center" />
+                </LineChart>
+              </ResponsiveContainer>
+              :
+              // If we're in list view
+              <ul className='ListView'>
+                {data.map((data) => (
+                  <li className='ListItem' key={data.timestamp}>
+                    <div>{convertUnixTimestampFull(data.unix_timestamp)}:</div>
+                    <div>{data.temp}°F {data.rh}%RH</div>
+                  </li>
+                  )
+                  )}
+              </ul>
+            }
+
+
           </>
         }
 
       </header>
     </div>
   );
-
 }
+
 
 export default App;
