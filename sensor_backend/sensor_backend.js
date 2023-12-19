731b2316d8cc780232213c846ce151f76d4b1560
dor219 <dor219@lehigh.edu>
2023-12-18 20:42:49 -0500

    formatting / responds to data add with current time for sensors to adjust their internal RTC

--- sensor_backend/sensor_backend.js
+++ sensor_backend/sensor_backend.js
@@ -2,14 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const pg = require('pg');
 const app = express();
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-// app.use(express.json());
+const body_parser = require('body-parser');
+app.use(body_parser.json());
 
-const conString = process.env.MY_ELEPHANTSQL_URL;
-const db = new pg.Client(conString);
+const pg_client = process.env.MY_ELEPHANTSQL_URL;
+const db = new pg.Client(pg_client);
 
 const port = 3333;
+
+
+// Connect to the database on startup.
+// Crashes the whole app if unable to connect to the database, so it can be restarted
+databaseConnect();
+
 
 // Start listening for any incoming traffic.
 app.listen(port, () => {
@@ -20,36 +25,7 @@ app.listen(port, () => {
 /**
  * Display index.html as landing page to show the app is running.
  */
-app.get('/',function(req, res) {
-    res.sendFile('index.html', { root: __dirname });
-});
-  
-  
-// Connect to the database.
-databaseConnect();
-
-
-/**
- * Crashes the whole app if unable to connect to the database.
- * App daemon would be restarted by pm2 to ensure uptime.
- */
-function databaseConnect() {
-    console.log("Attempting to connect to the database");
-
-    db.connect(function(err) {
-        if(err) {
-            console.error(`Could not connect to postgres: ${err}`);
-            console.log("Aborting execution due to postgres connection failure");
-            process.exit(1);
-        }
-    });
-}
-
-  
-/**
- * Display index.html as landing page to show the app is running.
- */
-app.get('/',function(req, res) {
+app.get('/', (req, res) => {
     res.sendFile('index.html', { root: __dirname });
 });
 
@@ -65,6 +41,12 @@ app.get('/',function(req, res) {
  *  "temp": <temperature_value>, // in F since rounding makes F a more precise value
  *  "rh": <%rh_value>
  * }
+ * 
+ * Replies to the request with the current time as plain text of the form:
+ * "Success:HH:MM:SS"
+ * Where HH is the current hour
+ * MM is the current minute
+ * SS is the current amount of seconds
  */
 app.post('/data', (req, res) => {
 
@@ -86,22 +68,29 @@ app.post('/data', (req, res) => {
     // Passes SQL query to ElephantSQL connection
     db.query(my_query, function(err, result) {
       if(err) {
+        res.status(500).send(`Error adding (${data.temp}, ${data.rh}) to TempRH_${data.id}`);  
         return console.error(`Error running query: ${err}`);
       }
-      res.status(200).send(`Successfully added (${data.temp}, ${data.rh}) to TempRH_${data.id}`);
+
+      const formattedTime = getHHMMSS();
+      res.status(200).send(`Success:${formattedTime}`);
     });
 });
 
+
 /**
- * Gets the current timestamp in the proper format to use as a SQL timestamp
+ * Crashes the whole app if unable to connect to the database, so it can be restarted.
  */
-function getTimestamp() {
-    const now = new Date();
-    // Format the date as a string in the 'YYYY-MM-DD HH:mm:ss' format
-    //  1.  now.toISOString() generates a string representation of the date in the ISO format: 'YYYY-MM-DDTHH:mm:ss.sssZ'.
-    //  2.  .replace('T', ' ') replaces the 'T' character with a space.
-    //  3.  .replace(/\.\d{3}Z$/, '') removes the milliseconds and the 'Z' character at the end.
-    return now.toISOString().replace('T', ' ').replace(/\.\d{3}Z$/, '');  
+function databaseConnect() {
+    console.log("Attempting to connect to the database");
+
+    db.connect(function(err) {
+        if(err) {
+            console.error(`Could not connect to postgres: ${err}`);
+            console.log("Aborting execution due to postgres connection failure");
+            process.exit(1);
+        }
+    });
 }
   
 
@@ -116,13 +105,38 @@ function createTable(id) {
         + `TempRH_${id} (`
         + "ID SERIAL PRIMARY KEY,"
         + "timestamp timestamp,"
-        + "temp decimal(5,2),"
-        + "rh decimal(5,2)"
+        + "temp decimal(4,1),"
+        + "rh decimal(4,1)"
         + ");";
 
     // Runs query to create table
-    db.query(my_query, function(err, result) {
+    db.query(my_query, (err, result) => {
         if(err)
             console.error(`Error creating TempRH table: ${err}`);
     });
+}
+
+
+/**
+ * Gets the current timestamp in the proper format to use as a SQL timestamp
+ */
+function getTimestamp() {
+    const now = new Date();
+    // Format the date as a string in the 'YYYY-MM-DD HH:mm:ss' format
+    //  1.  now.toISOString() generates a string representation of the date in the ISO format: 'YYYY-MM-DDTHH:mm:ss.sssZ'.
+    //  2.  .replace('T', ' ') replaces the 'T' character with a space.
+    //  3.  .replace(/\.\d{3}Z$/, '') removes the milliseconds and the 'Z' character at the end.
+    return now.toISOString().replace('T', ' ').replace(/\.\d{3}Z$/, '');  
+}
+
+
+/**
+ * Gets the current time formatted as HH:MM:SS to send to the sensor board.
+ */
+function getHHMMSS() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
 }
