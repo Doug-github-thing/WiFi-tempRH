62547237c1b179e4ae92ac6ceb185fb7746a0874
Doug-github-thing <gaflivinburger@gmail.com>
2024-06-04 18:47:36 -0400

    updated to listen for tcp connections instead of http requests

--- sensor_backend/sensor_backend.js
+++ sensor_backend/sensor_backend.js
@@ -1,142 +1,43 @@
-require('dotenv').config();
-const express = require('express');
-const pg = require('pg');
-const app = express();
-const body_parser = require('body-parser');
-app.use(body_parser.json());
+const net = require('net');
+const port = 55555;
+const host = '192.168.0.39';
 
-const pg_client = process.env.MY_ELEPHANTSQL_URL;
-const db = new pg.Client(pg_client);
+// https://www.digitalocean.com/community/tutorials/how-to-develop-a-node-js-tcp-server-application-using-pm2-and-nginx-on-ubuntu-16-04
 
-const port = 3333;
-
-
-// Connect to the database on startup.
-// Crashes the whole app if unable to connect to the database, so it can be restarted
-databaseConnect();
-
-
-// Start listening for any incoming traffic.
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+const server = net.createServer();
+server.listen(port, host, () => {
+    console.log('TCP Server is running on port ' + port +'.');
 });
 
+let sockets = [];
 
-/**
- * Display index.html as landing page to show the app is running.
- */
-app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: __dirname });
-});
+server.on('connection', (sock) => {
+    console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+    sockets.push(sock);
 
 
-/**
- * Listen for incoming POST traffic from the sensor module with new data to push the the database.
- * Expects a POST in the form: 
- * POST '/data'
- * Content-type: application/JSON
- * Body:
- * {
- *  "id": <sensor_module_id>, // Unique identifier for the sensor module
- *  "temp": <temperature_value>, // in F since rounding makes F a more precise value
- *  "rh": <%rh_value>
- * }
- * 
- * Replies to the request with the current time as plain text of the form:
- * "Success:HH:MM:SS"
- * Where HH is the current hour
- * MM is the current minute
- * SS is the current amount of seconds
- */
-app.post('/data', (req, res) => {
+    // When a connected client sends data, print it to the console, and respond that it got it.
+    sock.on('data', (data) => {
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        
+        // Write the data back to all the connected, the client will receive it as data from the server
+        sockets.forEach((sock, index, array) => {
+            sock.write(sock.remoteAddress + ':' + sock.remotePort + " said " + data + '\n');
+        });
+    });
 
-    // Parses values from incoming data from sensor module, 
-    // and current timestamp to send to the database
-    const timestamp = getTimestamp();
-    const data = req.body;
 
-    // Makes sure the table exists before attempting to add data to it
-    createTable(data.id);
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', (data) => {
 
-    // Builds SQL query
-    const my_query = `INSERT INTO TempRH_${data.id} (timestamp, temp, rh) VALUES (`
-                    + `'${timestamp}',`
-                    + `'${data.temp}',`
-                    + `'${data.rh}');`;
-    console.log("Attempting to run: " + my_query);
+        let index = sockets.findIndex((o) => {
+            return o.remoteAddress === sock.remoteAddress && o.remotePort === sock.remotePort;
+        })
 
-    // Passes SQL query to ElephantSQL connection
-    db.query(my_query, function(err, result) {
-      if(err) {
-        res.status(500).send(`Error adding (${data.temp}, ${data.rh}) to TempRH_${data.id}`);  
-        return console.error(`Error running query: ${err}`);
-      }
+        if (index !== -1)
+            sockets.splice(index, 1);
 
-      const formattedTime = getHHMMSS();
-      res.status(200).send(`Success:${formattedTime}`);
+        console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
     });
 });
 
-
-/**
- * Crashes the whole app if unable to connect to the database, so it can be restarted.
- */
-function databaseConnect() {
-    console.log("Attempting to connect to the database");
-
-    db.connect(function(err) {
-        if(err) {
-            console.error(`Could not connect to postgres: ${err}`);
-            console.log("Aborting execution due to postgres connection failure");
-            process.exit(1);
-        }
-    });
-}
-  
-
-/**
- * @param id The ID of the sensor module
- * 
- * Creates if not exists a table called "TempRH_<id>"
- */
-function createTable(id) {
-    // Creates the TempRH table if it does not exist
-    const my_query = "CREATE TABLE IF NOT EXISTS "
-        + `TempRH_${id} (`
-        + "ID SERIAL PRIMARY KEY,"
-        + "timestamp timestamp,"
-        + "temp decimal(4,1),"
-        + "rh decimal(4,1)"
-        + ");";
-
-    // Runs query to create table
-    db.query(my_query, (err, result) => {
-        if(err)
-            console.error(`Error creating TempRH table: ${err}`);
-    });
-}
-
-
-/**
- * Gets the current timestamp in the proper format to use as a SQL timestamp
- */
-function getTimestamp() {
-    const now = new Date();
-    // Format the date as a string in the 'YYYY-MM-DD HH:mm:ss' format
-    //  1.  now.toISOString() generates a string representation of the date in the ISO format: 'YYYY-MM-DDTHH:mm:ss.sssZ'.
-    //  2.  .replace('T', ' ') replaces the 'T' character with a space.
-    //  3.  .replace(/\.\d{3}Z$/, '') removes the milliseconds and the 'Z' character at the end.
-    return now.toISOString().replace('T', ' ').replace(/\.\d{3}Z$/, '');  
-}
-
-
-/**
- * Gets the current time formatted as HH:MM:SS to send to the sensor board.
- */
-function getHHMMSS() {
-    const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    return `${hours}:${minutes}:${seconds}`;
-}
