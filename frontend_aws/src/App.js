3213f369019021a475240ea06773baea5555874e
Doug-github-thing <gaflivinburger@gmail.com>
2024-06-19 21:47:10 -0400

    removed old comments

--- frontend_aws/src/App.js
+++ frontend_aws/src/App.js
@@ -30,27 +30,3 @@ const App = () => {
 }
 
 export default App;
-
-
-// import React from 'react';
-// import './App.css';
-// // // For Current temp/rh reading
-// // import { CurrentReadout } from './components/CurrentReadout.js';
-// // // For historical data visualization
-// // import { HistoricalDataPlot } from './components/HistoricalDataPlot.js';
-// // import snowflake from './snowflaketime.svg'; // Snowflake icon
-
-// const App = () => {
-//       // Define overall app skeleton
-//       return (
-//             <div className="App">
-//                   <header className="App-header">
-//                         <CurrentReadout />
-//                         <img src={snowflake} className="App-logo" alt="SMOW" />
-//                         <HistoricalDataPlot title={"Past 24 hours:"}></HistoricalDataPlot>
-//               </header>
-//         </div>
-//       );
-// }
-
-// export default App;
