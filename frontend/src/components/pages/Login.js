c186ca3f3d9bb590797d21a9030d4fc94eccc23c
Doug-github-thing <dougrynar@gmail.com>
2025-01-13 17:38:29 -0500

    added 'signin with google' button. As of yet, only gets a JWT and prints to console, doens't send jwt to backend or do anything with successful auth

--- frontend/src/components/pages/Login.js
+++ frontend/src/components/pages/Login.js
@@ -1,6 +1,7 @@
 import React, {useContext} from 'react';
 import { useSetState } from 'react-use';
 import { AuthContext } from '../../context/Auth.context.js';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import "./Login.css";
 
 const initialState = {
@@ -25,36 +26,63 @@ const LoginForm = () => {
         });
     }
 
-        
+
+    const handleSuccess = (credentialResponse) => {
+        // Handle the successful login here
+        console.log("Google login successful", credentialResponse);
+        console.log("Google ID token:", credentialResponse.credential);
+        console.log("The backend should use GoogleID JWT verifier to decode this to find its id");
+    };
+    
+
+    const handleError = () => {
+        // Handle login errors here
+        console.log("Google login failed");
+    };
+
+
     return (
-        <form name="loginForm" onSubmit={onSubmit}>
-
-            <div>Please sign in</div>
-
-            <div className="form">
-                <div><label htmlFor="username">Username</label></div>
-                <input 
-                    type="text" 
-                    name="username" 
-                    onChange={e => setState({email: e.target.value})} 
-                    value={state.email} 
-                    placeholder="admin" 
-                />
-                <div><label htmlFor="password">Password</label></div>
-                <input 
-                    type="password" 
-                    name="password" 
-                    onChange={e => setState({password: e.target.value})} 
-                    value={state.password} 
-                />
-                <div><input className="primary" type="submit" value="Login" /></div>                
+        <GoogleOAuthProvider clientId="695252668802-39ku4227n2vhsa7d2j6if6adnf8ls251.apps.googleusercontent.com">
+            <div>
+            <GoogleLogin
+                onSuccess={handleSuccess}
+                onError={handleError}
+                // Optionally, you can customize the button appearance and behavior
+            />
             </div>
+        </GoogleOAuthProvider>
+    );
 
-        { isLoginPending && <div>Please wait...</div> }
-        { isLoggedIn && <div>Success.</div> }
-        { loginError && <div>{loginError.message}</div> }
-        </form>
-    )
+        
+    // return (
+    //     <form name="loginForm" onSubmit={onSubmit}>
+
+    //         <div>Please sign in</div>
+
+    //         <div className="form">
+    //             <div><label htmlFor="username">Username</label></div>
+    //             <input 
+    //                 type="text" 
+    //                 name="username" 
+    //                 onChange={e => setState({email: e.target.value})} 
+    //                 value={state.email} 
+    //                 placeholder="admin" 
+    //             />
+    //             <div><label htmlFor="password">Password</label></div>
+    //             <input 
+    //                 type="password" 
+    //                 name="password" 
+    //                 onChange={e => setState({password: e.target.value})} 
+    //                 value={state.password} 
+    //             />
+    //             <div><input className="primary" type="submit" value="Login" /></div>                
+    //         </div>
+
+    //     { isLoginPending && <div>Please wait...</div> }
+    //     { isLoggedIn && <div>Success.</div> }
+    //     { loginError && <div>{loginError.message}</div> }
+    //     </form>
+    // )
 }
 
 
