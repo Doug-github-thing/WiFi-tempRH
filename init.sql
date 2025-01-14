c87f6551cdd402da71a3cbf92b95493328f15fd1
Doug-github-thing <dougrynar@gmail.com>
2025-01-14 00:13:41 -0500

    added nodes number tracking to db to make it easier to make new ones

--- init.sql
+++ init.sql
@@ -4,10 +4,18 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(50),      -- User's email, the part before the @
     valid_nodes VARCHAR(32) -- Comma separated list of nodes owned by this user
 );
-INSERT INTO monitorDB.users VALUES (NULL, 'Doug', 'dougrynar', '0,1');
+INSERT INTO monitorDB.users VALUES (NULL, 'Doug', 'dougrynar', '1,2');
 
 
-CREATE TABLE IF NOT EXISTS node_0 (
+CREATE TABLE IF NOT EXISTS monitorDB.nodes (
+    node_id INT AUTO_INCREMENT PRIMARY KEY,
+    owner_id INT,           -- user_id of the owner of this node
+    name VARCHAR(50)        -- Name of node location
+);
+INSERT INTO monitorDB.nodes VALUES (NULL, 1, "Doug's house");
+
+
+CREATE TABLE IF NOT EXISTS monitorDB.node_1 (
     id INT AUTO_INCREMENT PRIMARY KEY,
     sensor_id INT,
     timestamp TIMESTAMP,
@@ -16,19 +24,19 @@ CREATE TABLE IF NOT EXISTS node_0 (
 );
 
 
-CREATE TABLE IF NOT EXISTS node_0_sensors (
+CREATE TABLE IF NOT EXISTS monitorDB.node_1_sensors (
     sensor_id INT PRIMARY KEY,
     name VARCHAR(50)
 );
-INSERT INTO node_0_sensors (sensor_id, name) VALUES (1, 'Game Room');
-INSERT INTO node_0_sensors (sensor_id, name) VALUES (2, 'Living Room');
-INSERT INTO node_0_sensors (sensor_id, name) VALUES (3, 'Bed Room');
-INSERT INTO node_0_sensors (sensor_id, name) VALUES (4, 'Porch');
+INSERT INTO monitorDB.node_1_sensors (sensor_id, name) VALUES (1, 'Game Room');
+INSERT INTO monitorDB.node_1_sensors (sensor_id, name) VALUES (2, 'Living Room');
+INSERT INTO monitorDB.node_1_sensors (sensor_id, name) VALUES (3, 'Bed Room');
+INSERT INTO monitorDB.node_1_sensors (sensor_id, name) VALUES (4, 'Porch');
 
 
 -- For tracking currently active session IDs. 
 -- Default expiry is set to 1 hour after entry is made
-CREATE TABLE IF NOT EXISTS session_ids (
+CREATE TABLE IF NOT EXISTS monitorDB.session_ids (
     session_id VARCHAR(32) PRIMARY KEY,  -- This session ID
     user_id INT,                         -- ID of the user this session belongs to
     valid_nodes TEXT,                    -- Comma separated list of nodes owned by this user
